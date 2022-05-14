aaa3a5377bbf96c2fbf4fb6c1998b9af4cafb63d
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2022-05-14 12:45:28 +0300

    fixed a bit DatabaseConfiguration: renamed scan package fixed a bit RestConfiguration: renamed scan package

--- db/src/main/java/vsu/javablog/db/DatabaseConfiguration.java
+++ db/src/main/java/vsu/javablog/db/DatabaseConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EntityScan(basePackages = {
     "vsu.javablog.db.entity"
 })
-@EnableJpaRepositories(basePackages = "vsu.javablog.db.repository")
+@EnableJpaRepositories(basePackages = "vsu.javablog.db.repositories")
 @EnableTransactionManagement
 public class DatabaseConfiguration {
 }
