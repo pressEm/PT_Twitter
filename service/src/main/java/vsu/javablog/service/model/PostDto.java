c1e004d74068c4141ab872efbf2b614ec2bf3c5e
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2022-05-14 15:17:21 +0300

    fixed UserEntity again? don't remember added simple Dtos: PostDto, CommentDto, TagDto

--- service/src/main/java/vsu/javablog/service/model/PostDto.java
+++ service/src/main/java/vsu/javablog/service/model/PostDto.java
@@ -1,4 +1,29 @@
 package vsu.javablog.service.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.List;
+
+@Getter
+@Setter
 public class PostDto {
+    @JsonProperty("title")
+    @NotBlank(message = "title.is-blank")
+    @Size(min = 1, max = 50, message = "title.out-of-range")
+    private String title;
+
+    @JsonProperty("content")
+    @NotBlank(message = "content.is-blank")
+    @Size(min = 1, max = 1000, message = "content.out-of-range")
+    private String content;
+
+    @JsonProperty("user_id")
+    private Integer userId;
+
+    private List<TagDto> tags;
+    private List<CommentDto> comments;
 }
