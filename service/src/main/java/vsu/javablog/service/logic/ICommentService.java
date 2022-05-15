f4b4e9af50d97659a885b02219a3c78dc95cb1cf
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2022-05-15 13:32:44 +0300

    first scratch of methods in interfaces of services

---
+++ service/src/main/java/vsu/javablog/service/logic/ICommentService.java
@@ -0,0 +1,16 @@
+package vsu.javablog.service.logic;
+
+import vsu.javablog.service.model.CommentDto;
+
+import javax.validation.Valid;
+import java.util.List;
+
+public interface ICommentService {
+    CommentDto createComment(@Valid CommentDto dto);
+
+    CommentDto getCommentById(Integer id);
+
+    List<CommentDto> getAllCommentsByUserId();
+
+    List<CommentDto> getAllCommentsByPostId();
+}
