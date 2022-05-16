1ac16fd242b39bab72be3f2796e0deafa0145b67
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2022-05-16 13:45:08 +0300

    TagMapper

--- service/src/main/java/vsu/javablog/service/mapper/impl/TagMapper.java
+++ service/src/main/java/vsu/javablog/service/mapper/impl/TagMapper.java
@@ -4,21 +4,31 @@ import vsu.javablog.db.entities.TagEntity;
 import vsu.javablog.service.mapper.ITagMapper;
 import vsu.javablog.service.model.TagDto;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class TagMapper implements ITagMapper {
     @Override
     public TagDto fromEntity(TagEntity entity) {
-        return null;
+        TagDto dto = new TagDto();
+        dto.setTag(entity.getTag());
+        return dto;
     }
 
     @Override
     public TagEntity toEntity(TagDto dto) {
-        return null;
+        TagEntity e = new TagEntity();
+        e.setTag(dto.getTag());
+        return e;
     }
 
     @Override
     public List<TagDto> fromEntities(Iterable<TagEntity> entities) {
-        return null;
+        List<TagDto> l = new LinkedList<>();
+        for (TagEntity e :
+            entities) {
+            l.add(fromEntity(e));
+        }
+        return l;
     }
 }
